dec58fef54ea6a5e702c86974df4d91d13ec51cb
Theo Wright <theowright2015@gmail.com>
2023-11-24 14:48:51 +0000

    added components

---
+++ host/apps/base/src/Base.tsx
@@ -0,0 +1,10 @@
+
+
+import React from 'react';
+import {Sub} from 'sub'
+export const Base = () => (
+	<div>
+		<div>Base Component</div>
+        <Sub/>
+	</div>
+);
---
+++ host/libraries/sub/src/Sub.jsx
@@ -0,0 +1,3 @@
+export const Sub = () => (
+    <div>Sub Component</div>
+)
